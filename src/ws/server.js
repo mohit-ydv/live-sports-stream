2daca6215724328127a2163bb009a53340c71f7f
Mohit Yadav <mohityadav@Mohits-MacBook-Air.local>
2026-05-24 11:38:05 +0530

    implement arcjet security

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import {WebSocket, WebSocketServer} from 'ws';
+import {httpArcjet, wsArcjet} from "../arcjet.js";
 
 function sendJson(socket, payload){
     if(socket.readyState !== WebSocket.OPEN) return;
@@ -20,7 +21,25 @@ export function attachWebSocketServer(server){
         path: '/ws',
         maxPayload: 1024 * 1024,
     });
-    wss.on('connection', (socket) => {
+    wss.on('connection', async (socket, req) => {
+        // Arcjet will protect ws server
+        if(wsArcjet){
+            try{
+                const decision = await wsArcjet.protect(req);
+                if(decision.isDenied()){
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008;
+                    const reason = decision.reason.isRateLimit() ? 'Rate limit exceeded' : 'Access denied';
+
+                    socket.close(code, reason);
+                    return;
+                }
+            }catch(e){
+                console.error('ws connection error', e);
+                socket.close(1011, 'Server security error.');
+                return;
+            }
+        }
+
         socket.isAlive = true;
         socket.on('pong', () => {socket.isAlive = true});
 
